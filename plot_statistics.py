1f3547dea4b656c5a7c44153c012613a9dacdd6d
Cenk Tüysüz <cenktuysuz@gmail.com>
2019-10-06 19:14:35 +0300

    csv plot support

--- plot_statistics.py
+++ plot_statistics.py
@@ -1,17 +1,35 @@
 import matplotlib.pyplot as plt 
 import numpy as np
+import csv
 
-# Plot the result every update  
+with open('logs/log_loss.csv', 'r') as f:
+	reader = csv.reader(f, delimiter=',')
+	loss = np.array(list(reader)).astype(float)
+with open('logs/log_validation.csv', 'r') as f:
+	reader = csv.reader(f, delimiter=',')
+	valid = np.array(list(reader)).astype(float) * 100
+with open('logs/log_theta.csv','r') as f:
+	reader = csv.reader(f, delimiter=',')
+	theta = np.delete(np.array(list(reader)[:-2]),11,1).astype(float) 
+# Plot
 plt.clf()   
-x = [(i+1) for i  in range(n_file+1)]
-plt.plot(x,loss_log[:n_file+1],marker='o')
+x = [(i+1) for i  in range(len(loss))]
+plt.plot(x,loss,marker='o')
 plt.xlabel('Update')
 plt.ylabel('Loss')
-plt.savefig('png\statistics_loss.png')
+plt.savefig('png/statistics_loss.png')
+
+plt.clf()   
+x = [i*50 for i  in range(len(valid))]
+plt.plot(x,valid,marker='o')
+plt.xlabel('Update')
+plt.ylabel('Accuracy %')
+plt.savefig('png/statistics_validation.png')
 
 plt.clf()
+x = [(i+1) for i in range(len(theta))]
 for i in range(11):
-	plt.plot(x,theta_log[:n_file+1,i],marker='o',label=r'$\theta_{'+str(i)+'}$')
+	plt.plot(x,theta[:,i],marker='o',label=r'$\theta_{'+str(i)+'}$')
 plt.xlabel('Update')
 plt.ylabel(r'Angle (0 - 2$\pi$)')
-plt.savefig('png\statistics_angle.png')
+plt.savefig('png/statistics_angle.png')
