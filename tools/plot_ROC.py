809f2b6b9f6558bbdcaeff7e3c813d73768d7231
Cenk Tuysuz <ctuysuz@culture-plate-sm.hep.caltech.edu>
2019-10-31 05:24:17 -0700

    ROC with sklearn

--- tools/plot_ROC.py
+++ tools/plot_ROC.py
@@ -1,34 +1,20 @@
 import matplotlib.pyplot as plt 
 import numpy as np
 import csv
-
+from sklearn import metrics
 log_location = 'logs/'
 png_location = 'png/'
 with open(log_location + 'log_validation_preds.csv', 'r') as f:
 	reader = csv.reader(f, delimiter=',')
 	preds = np.array(list(reader)).astype(float)
 
-threshold = np.linspace(0,1,50)
-TP = np.zeros(len(threshold))
-TN = np.zeros(len(threshold))
-FP = np.zeros(len(threshold))
-FN = np.zeros(len(threshold))
-for idx,val in enumerate(threshold):
-	for pred in preds:
-		if pred[0] >= val and pred[1]==1.:
-			TP[idx] += 1.
-		elif pred[0] >= val and pred[1]==0.:
-			FP[idx] += 1.
-		elif pred[0] < val and pred[1]==1.:
-			FN[idx] += 1.
-		elif pred[0] < val and pred[1]==0.:
-			TN[idx] += 1.
-TPR = TP / (TP+FN)
-FPR = FP / (FP+TN)
+fpr,tpr,thresholds = metrics.roc_curve(preds[:,1].astype(int),preds[:,0],pos_label=1 )
+auc = metrics.auc(fpr,tpr)
+print('AUC: ' + str(auc))
 # Plot
 plt.clf()   
-plt.scatter(FPR,TPR,c='blue',marker='.')
-plt.scatter(threshold,threshold,c='orange',marker='D')
+plt.plot(fpr,tpr,c='darkorange')
+plt.plot([0, 1], [0, 1], color='navy', linestyle='--')
 plt.xlabel('False Positive Rate')
 plt.ylabel('True Positive Rate')
 plt.title('ROC Curve')
