606a991b2883fd0a3c5643da7bca28aa586f8c68
Cenk Tüysüz <cenktuysuz@gmail.com>
2019-11-05 12:01:56 +0100

    minor changes

--- tools/plot_statistics.py
+++ tools/plot_statistics.py
@@ -1,49 +1,73 @@
 import matplotlib.pyplot as plt 
 import numpy as np
 import csv
+from sklearn import metrics
 
-log_location = 'logs/bce/lr_1/'
-png_location = 'png/bce/lr_1/'
+log_location = 'logs/TTN/lr_0_1/'
+png_location = 'png/TTN/lr_0_1/'
 with open(log_location + 'log_loss.csv', 'r') as f:
 	reader = csv.reader(f, delimiter=',')
 	loss = np.array(list(reader)).astype(float)
 with open(log_location+'log_validation.csv', 'r') as f:
 	reader = csv.reader(f, delimiter=',')
-	valid = np.array(list(reader)).astype(float) * 100
+	valid = np.array(list(reader)).astype(float) 
 with open(log_location+'log_theta.csv','r') as f:
 	reader = csv.reader(f, delimiter=',')
-	theta = np.delete(np.array(list(reader)[:-2]),11,1).astype(float) 
-# Plot
+	theta = np.delete(np.array(list(reader)[:-2]),11,1).astype(float)
+with open(log_location + 'log_validation_preds.csv', 'r') as f:
+	reader = csv.reader(f, delimiter=',')
+	preds = np.array(list(reader)).astype(float)
+
+# Plots
+
 plt.clf()   
 x = [(i+1) for i  in range(len(loss))]
-plt.plot(x[1:],loss[1:],marker='o')
+plt.plot(x[1:],loss[1:])
 plt.xlabel('Update')
 plt.ylabel('Loss')
 plt.savefig(png_location+'statistics_loss.pdf')
 
 plt.clf()   
 interval = 50
-x = [i*interval for i  in range(len(loss)//interval)]
-average_loss = [sum(loss[i*interval:(i+1)*interval])/interval for i in range(len(x))]
-plt.plot(x,average_loss,marker='o')
+x = [i*interval for i  in range(len(valid))]
+plt.plot(x,valid[:,2])
 plt.xlabel('Update')
 plt.ylabel('Loss')
-plt.savefig(png_location+'statistics_average_loss.pdf')
+plt.savefig(png_location+'validation_loss.pdf')
 
 plt.clf()   
 x = [i*50 for i  in range(len(valid))]
-plt.plot(x,valid,marker='o')
+plt.plot(x,valid[:,0]*100)
 plt.xlabel('Update')
 plt.ylabel('Accuracy')
-plt.savefig(png_location+'statistics_validation.pdf')
+plt.savefig(png_location+'validation_accuracy.pdf')
+
+plt.clf()   
+x = [i*50 for i  in range(len(valid))]
+plt.plot(x,valid[:,1])
+plt.xlabel('Update')
+plt.ylabel('AUC')
+plt.savefig(png_location+'validation_auc.pdf')
 
 plt.clf()
 x = [(i+1) for i in range(len(theta))]
 for i in range(11):
-	plt.plot(x,theta[:,i],marker='o',label=r'$\theta_{'+str(i)+'}$')
+	plt.scatter(x,theta[:,i],marker='.',label=r'$\theta_{'+str(i)+'}$')
 plt.xlabel('Update')
 plt.ylabel(r'Angle (0 - 2$\pi$)')
 plt.savefig(png_location+'statistics_angle.pdf')
+
+
+fpr,tpr,thresholds = metrics.roc_curve(preds[:,1].astype(int),preds[:,0],pos_label=1 )
+auc = metrics.auc(fpr,tpr)
+# Plot
+plt.clf()   
+plt.plot(fpr,tpr,c='navy')
+plt.plot([0, 1], [0, 1], color='darkorange', linestyle='--')
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('ROC Curve')
+plt.savefig(png_location+'ROC.pdf') 
 """
 # Comparison plot
 plt.clf()   
