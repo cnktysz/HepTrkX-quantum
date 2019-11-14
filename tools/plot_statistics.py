1d1b502159ee243d686f83febfcb9110b40a03a8
Cenk Tüysüz <cenktuysuz@gmail.com>
2019-11-14 09:55:39 +0300

    plot udpates

--- tools/plot_statistics.py
+++ tools/plot_statistics.py
@@ -3,8 +3,8 @@ import numpy as np
 import csv
 from sklearn import metrics
 
-log_location = 'logs/pennylane/MERA/lr_0_1/'
-png_location = 'png/pennylane/MERA/lr_0_1/'
+log_location = 'logs/MERA/lr_0_1/'
+png_location = 'png/MERA/lr_0_1/'
 circuit_type = 'MERA'
 if circuit_type=='TTN':
 	n_param = 11
@@ -21,10 +21,15 @@ with open(log_location+'log_theta.csv','r') as f:
 	theta = np.delete(np.array(list(reader)[:-2]),n_param,1).astype(float)
 with open(log_location + 'log_validation_preds.csv', 'r') as f:
 	reader = csv.reader(f, delimiter=',')
-	preds = np.array(list(reader)).astype(float)
+	valid_preds = np.array(list(reader)).astype(float)
+with open(log_location+'log_training.csv', 'r') as f:
+	reader = csv.reader(f, delimiter=',')
+	train = np.array(list(reader)).astype(float) 
+with open(log_location + 'log_training_preds.csv', 'r') as f:
+	reader = csv.reader(f, delimiter=',')
+	train_preds = np.array(list(reader)).astype(float)
 
 # Plots
-
 plt.clf()   
 x = [(i+1) for i  in range(len(loss))]
 plt.plot(x[1:],loss[1:])
@@ -32,6 +37,16 @@ plt.xlabel('Update')
 plt.ylabel('Loss')
 plt.savefig(png_location+'statistics_loss.pdf')
 
+plt.clf()
+x = [(i+1) for i in range(len(theta))]
+for i in range(n_param):
+	plt.scatter(x,theta[:,i],marker='.',label=r'$\theta_{'+str(i)+'}$')
+plt.xlabel('Update')
+plt.ylabel(r'Angle (0 - 2$\pi$)')
+plt.savefig(png_location+'statistics_angle.pdf')
+
+
+# Validation Plots
 plt.clf()   
 interval = 50
 x = [i*interval for i  in range(len(valid))]
@@ -54,16 +69,8 @@ plt.xlabel('Update')
 plt.ylabel('AUC')
 plt.savefig(png_location+'validation_auc.pdf')
 
-plt.clf()
-x = [(i+1) for i in range(len(theta))]
-for i in range(n_param):
-	plt.scatter(x,theta[:,i],marker='.',label=r'$\theta_{'+str(i)+'}$')
-plt.xlabel('Update')
-plt.ylabel(r'Angle (0 - 2$\pi$)')
-plt.savefig(png_location+'statistics_angle.pdf')
 
-
-fpr,tpr,thresholds = metrics.roc_curve(preds[:,1].astype(int),preds[:,0],pos_label=1 )
+fpr,tpr,thresholds = metrics.roc_curve(valid_preds[:,1].astype(int),valid_preds[:,0],pos_label=1 )
 auc = metrics.auc(fpr,tpr)
 # Plot
 plt.clf()   
@@ -72,20 +79,41 @@ plt.plot([0, 1], [0, 1], color='darkorange', linestyle='--')
 plt.xlabel('False Positive Rate')
 plt.ylabel('True Positive Rate')
 plt.title('ROC Curve')
-plt.savefig(png_location+'ROC.pdf') 
-"""
-# Comparison plot
+plt.savefig(png_location+'validation_roc.pdf') 
+
+# Training Plots
 plt.clf()   
 interval = 50
-x = [i*interval for i  in range(len(loss)//interval)]
-with np.load('logs/summaries.npz') as data:
-	classical_loss = data['train_loss'][:len(loss)//(interval*3)]
-average_loss = [sum(loss[i*interval:(i+1)*interval])/interval for i in range(len(x))]
-plt.plot(x,average_loss,marker='o',label='QuantumEdgeNetwork',c='r')
-xx = [i*interval*3 for i  in range(len(classical_loss))]
-plt.plot(xx,classical_loss,marker='o',label='ClassicalEdgeNetwork',c='b')
+x = [i*interval for i  in range(len(train))]
+plt.plot(x,valid[:,2])
 plt.xlabel('Update')
 plt.ylabel('Loss')
-plt.legend()
-plt.savefig(png_location+'statistics_comparison_loss.png')
-"""
+plt.savefig(png_location+'training_loss.pdf')
+
+plt.clf()   
+x = [i*50 for i  in range(len(train))]
+plt.plot(x,valid[:,0]*100)
+plt.xlabel('Update')
+plt.ylabel('Accuracy')
+plt.savefig(png_location+'training_accuracy.pdf')
+
+plt.clf()   
+x = [i*50 for i  in range(len(train))]
+plt.plot(x,valid[:,1])
+plt.xlabel('Update')
+plt.ylabel('AUC')
+plt.savefig(png_location+'training_auc.pdf')
+
+fpr,tpr,thresholds = metrics.roc_curve(train_preds[:,1].astype(int),train_preds[:,0],pos_label=1 )
+auc = metrics.auc(fpr,tpr)
+# Plot
+plt.clf()   
+plt.plot(fpr,tpr,c='navy')
+plt.plot([0, 1], [0, 1], color='darkorange', linestyle='--')
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('ROC Curve')
+plt.savefig(png_location+'training_roc.pdf') 
+
+
+
