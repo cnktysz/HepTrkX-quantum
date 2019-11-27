ed967f1d6d38c8e18df634ca631a4fcb1270331d
Cenk Tüysüz <cenktuysuz@gmail.com>
2019-11-27 16:33:09 +0300

    tensorflow layer strcuture, can't take gradient for now, returns None

--- tools/printNetwork.py
+++ tools/printNetwork.py
@@ -5,42 +5,15 @@ import matplotlib.pyplot as plt
 from qiskit import *
 from datasets.hitgraphs import get_datasets
 import multiprocessing
-from qnetworks.MERA import MERA_edge_forward
+from qnetworks.TTN8 import TTN_edge_forward
 
-def map2angle(B):
-	# Maps input features to 0-2PI
-	r_min     = 0.
-	r_max     = 1.
-	phi_min   = -1.
-	phi_max   = 1.
-	z_min     = 0.
-	z_max     = 1.2
-	B[:,0] =  (B[:,0]-r_min)/(r_max-r_min) * 2 * np.pi 
-	B[:,1] =  (B[:,1]-phi_min)/(phi_max-phi_min) * 2 * np.pi 
-	B[:,2] =  (B[:,2]-z_min)/(z_max-z_min) * 2 * np.pi 
-	B[:,3] =  (B[:,3]-r_min)/(r_max-r_min) * 2 * np.pi 
-	B[:,4] =  (B[:,4]-phi_min)/(phi_max-phi_min) * 2 * np.pi 
-	B[:,5] =  (B[:,5]-z_min)/(z_max-z_min) * 2 * np.pi 
-	return B
-def preprocess(data):
-	X,Ro,Ri,y = data
-	X[:,2] = np.abs(X[:,2]) # correction for negative z
-	bo    = np.dot(Ro.T, X)
-	bi    = np.dot(Ri.T, X)
-	B     = np.concatenate((bo,bi),axis=1)
-	return map2angle(B), y
 ############################################################################################
 ##### MAIN ######
 if __name__ == '__main__':
-	n_param = 19
+	n_param = 15
 	theta_learn = np.random.rand(n_param)*np.pi*2 
-	input_dir   = 'data/hitgraphs_big' 
-	n_files     = 2
-	n_valid     = 1
-	n_train     = 1
-	train_data, valid_data = get_datasets(input_dir, n_train, n_valid)
-	B, y = preprocess(train_data[0])
-	MERA_edge_forward(B[0],theta_learn,draw=True)
+	edge = np.random.rand(8) * np.pi * 2
+	TTN_edge_forward(edge,theta_learn,draw=True)
 	
 
 	
--- trying_things/tensorflow_layer.py
+++ trying_things/tensorflow_layer.py
@@ -1,101 +1,215 @@
 # Calculates gradients of a pennylane quantum circuit
 # using tensorflow
+import sys, os, time, datetime, csv
+sys.path.append(os.path.abspath(os.path.join('.')))
 import pennylane as qml 
 from pennylane import numpy as np
 import tensorflow as tf
-import sys
+from datasets.hitgraphs import get_datasets
 
-dev1 = qml.device("default.qubit", wires=6)
+
+dev1 = qml.device("default.qubit", wires=8)
 @qml.qnode(dev1,interface='tf')
 def TTN_edge_forward(edge,theta_learn):
 	# Takes the input and learning variables and applies the
 	# network to obtain the output
 	
 	# STATE PREPARATION
-	for i in range(len(edge)):
+	for i in range(8):
 		qml.RY(edge[i],wires=i)
 	# APPLY forward sequence
+	# First Layer
 	qml.RY(theta_learn[0],wires=0)
 	qml.RY(theta_learn[1],wires=1)
 	qml.CNOT(wires=[0,1])
 	qml.RY(theta_learn[2],wires=2)
 	qml.RY(theta_learn[3],wires=3)
-	qml.CNOT(wires=[2,3])
+	qml.CNOT(wires=[3,2])
 	qml.RY(theta_learn[4],wires=4)
 	qml.RY(theta_learn[5],wires=5)
-	qml.CNOT(wires=[5,4])
-	qml.RY(theta_learn[6],wires=1)
-	qml.RY(theta_learn[7],wires=3)
-	qml.CNOT(wires=[1,3])
-	qml.RY(theta_learn[8],wires=3)
-	qml.RY(theta_learn[9],wires=4)
-	qml.CNOT(wires=[3,4])
-	qml.RY(theta_learn[10],wires=4)		
-	return qml.expval(qml.PauliZ(wires=4))
-dev2 = qml.device("default.qubit", wires=6)
+	qml.CNOT(wires=[4,5])
+	qml.RY(theta_learn[6],wires=6)
+	qml.RY(theta_learn[7],wires=7)
+	qml.CNOT(wires=[7,6])
+	# Second Layer
+	qml.RY(theta_learn[8],wires=1)
+	qml.RY(theta_learn[9],wires=2)
+	qml.CNOT(wires=[1,2])
+	qml.RY(theta_learn[10],wires=5)
+	qml.RY(theta_learn[11],wires=6)
+	qml.CNOT(wires=[6,5])
+	# Third Layer
+	qml.RY(theta_learn[12],wires=2)
+	qml.RY(theta_learn[13],wires=5)
+	qml.CNOT(wires=[2,5])
+	#Last Layer
+	qml.RY(theta_learn[14],wires=5)		
+	return qml.expval(qml.PauliZ(wires=5))
+
+dev2 = qml.device("default.qubit", wires=12)
 @qml.qnode(dev2,interface='tf')
 def TTN_node_forward(edge,theta_learn):
 	# Takes the input and learning variables and applies the
 	# network to obtain the output
 	
 	# STATE PREPARATION
-	for i in range(len(edge)):
+	for i in range(12):
 		qml.RY(edge[i],wires=i)
 	# APPLY forward sequence
+	# First Layer
 	qml.RY(theta_learn[0],wires=0)
 	qml.RY(theta_learn[1],wires=1)
 	qml.CNOT(wires=[0,1])
 	qml.RY(theta_learn[2],wires=2)
 	qml.RY(theta_learn[3],wires=3)
-	qml.CNOT(wires=[2,3])
+	qml.CNOT(wires=[3,2])
 	qml.RY(theta_learn[4],wires=4)
 	qml.RY(theta_learn[5],wires=5)
-	qml.CNOT(wires=[5,4])
-	qml.RY(theta_learn[6],wires=1)
-	qml.RY(theta_learn[7],wires=3)
-	qml.CNOT(wires=[1,3])
-	qml.RY(theta_learn[8],wires=3)
-	qml.RY(theta_learn[9],wires=4)
-	qml.CNOT(wires=[3,4])
-	qml.RY(theta_learn[10],wires=4)		
-	return qml.expval(qml.PauliZ(wires=4))
+	qml.CNOT(wires=[4,5])
+	qml.RY(theta_learn[6],wires=6)
+	qml.RY(theta_learn[7],wires=7)
+	qml.CNOT(wires=[7,6])
+	qml.RY(theta_learn[8],wires=8)
+	qml.RY(theta_learn[9],wires=9)
+	qml.CNOT(wires=[8,9])
+	qml.RY(theta_learn[10],wires=10)
+	qml.RY(theta_learn[11],wires=11)
+	qml.CNOT(wires=[11,10])
+	# Second Layer
+	qml.RY(theta_learn[12],wires=1)
+	qml.RY(theta_learn[13],wires=2)
+	qml.CNOT(wires=[1,2])
+	qml.RY(theta_learn[14],wires=5)
+	qml.RY(theta_learn[15],wires=6)
+	qml.CNOT(wires=[6,5])
+	qml.RY(theta_learn[16],wires=9)
+	qml.RY(theta_learn[17],wires=10)
+	qml.CNOT(wires=[10,9])	
+	# Third Layer
+	qml.RY(theta_learn[18],wires=2)
+	qml.RY(theta_learn[19],wires=5)
+	qml.CNOT(wires=[2,5])	
+	# Forth Layer
+	qml.RY(theta_learn[20],wires=5)
+	qml.RY(theta_learn[21],wires=9)
+	qml.CNOT(wires=[5,9])
+	# Last Layer
+	qml.RY(theta_learn[22],wires=4)		
 
+	return qml.expval(qml.PauliZ(wires=9))
+#######################################
+def edge_forward(edge_array,theta_learn):
+	outputs = []
+	for i in range(len(edge_array[:,0])):
+		outputs.append(TTN_edge_forward(edge_array[i,:],theta_learn))
+	return (1-np.array(outputs))/2
+def node_forward(node_array,theta_learn):
+	outputs = []
+	for i in range(len(node_array[:,0])):
+		outputs.append(TTN_node_forward(node_array[i,:],theta_learn))
+	return (1-np.array(outputs))/2
+def map2angle(B):
+	# Maps input features to 0-2PI
+	r_min     = 0.
+	r_max     = 1.
+	phi_min   = -1.
+	phi_max   = 1.
+	z_min     = 0.
+	z_max     = 1.2
+	B[:,0] =  (B[:,0]-r_min)/(r_max-r_min) * 2 * np.pi 
+	B[:,1] =  (B[:,1]-phi_min)/(phi_max-phi_min) * 2 * np.pi 
+	B[:,2] =  (B[:,2]-z_min)/(z_max-z_min) * 2 * np.pi 
+	return B
+############################################################################################
+class EdgeNet(tf.keras.layers.Layer):
+	def __init__(self):
+		super(EdgeNet, self).__init__(name='EdgeNet')
+		self.theta_learn = tf.Variable(np.random.rand(15) * np.pi * 2,dtype=tf.float32)
+
+	def call(self,X, Ri, Ro):
+		bo = tf.matmul(Ro,X,transpose_a=True)
+		bi = tf.matmul(Ri,X,transpose_a=True)
+		B  = tf.concat([bo, bi], axis=1)  
+		return edge_forward(B,self.theta_learn)
+
+class NodeNet(tf.keras.layers.Layer):
+	def __init__(self):
+		super(NodeNet, self).__init__(name='NodeNet')
+		self.theta_learn = tf.Variable(np.random.rand(23) * np.pi * 2,dtype=tf.float32)
+
+	def call(self,X, e, Ri, Ro):
+		bo = tf.matmul(Ro,X,transpose_a=True)
+		bi = tf.matmul(Ri,X,transpose_a=True)
+		Rwo = Ro * e
+		Rwi = Ri * e
+		mi = tf.matmul(Rwi, bo)
+		mo = tf.matmul(Rwo, bi)
+		M = tf.concat([mi, mo, X], axis=1)
+		return node_forward(M,self.theta_learn)
+
+class InputNet(tf.keras.layers.Layer):
+  def __init__(self, num_outputs):
+    super(InputNet, self).__init__(name='InputNet')
+    self.num_outputs = num_outputs
+    self.kernel = tf.Variable(np.random.rand(3,num_outputs),dtype=tf.float32)
+
+  def call(self, input):
+    return tf.matmul(input, kernel)
+
+class GNN(tf.keras.Model):
+	def __init__(self):
+		super(GNN, self).__init__(name='GNN')
+		self.InputNet = InputNet(1)
+		self.EdgeNet0 = EdgeNet()
+		self.NodeNet = NodeNet()
+		self.EdgeNet1 = EdgeNet()
+
+	def call(self, inputs):
+		X, Ri, Ro = inputs
+		H = self.InputNet(X) # not normalized, be careful !
+		H = tf.concat([H,X],axis=1)
+		e = self.EdgeNet0(H, Ri, Ro)
+		H = self.NodeNet(H, e, Ri, Ro)
+		H = tf.concat([H[:,None],X],axis=1)
+		e = self.EdgeNet1(H, Ri, Ro)
+		return e
 def binary_cross_entropy(output,label):
 	return - label*tf.math.log(output+1e-6) - (1-label)*tf.math.log(1-output+1e-6) 
-
-def tf_grad(edge,theta_learn0,theta_learn1,label):
-
+def gradient(block,edge_array,label):
 	with tf.GradientTape() as tape:
-		output0 = (1 - TTN_edge_forward(edge,theta_learn0))/2
-		output1 = output0 + (1 - TTN_node_forward(edge,theta_learn1))/2
-		loss = binary_cross_entropy(output1,label)
-		grad0,grad1 = tape.gradient(loss,[theta_learn0,theta_learn1])
-	return grad0,grad1
+		tape.watch(block.trainable_variables[0])
+		output = block(edge_array)
+		loss = tf.reduce_mean(binary_cross_entropy(output,label))	
+		print(loss)
+	grad = tape.gradient(loss,block.trainable_variables[0])
+	print(grad)
+	return grad
 ############################################################################################
+
 if __name__ == '__main__':
 	tf.executing_eagerly()
-	#w = tf.Variable(tf.random.uniform([11,1],0,2*np.pi,dtype=tf.float64),dtype=tf.float64)
-	#theta_learn = np.array([0.59955824, 0.72293889, 0.76065828, 0.07674539, 0.22250796, 0.68152528, 0.72906506, 0.67450772, 0.51897852, 0.57968062, 0.3478734]) * 2 * np.pi
-	#w = tf.Variable(theta_learn,dtype=tf.float64)
-	w0 = tf.Variable(np.random.rand(11) * np.pi * 2,dtype=tf.float64)
-	w1 = tf.Variable(np.random.rand(11) * np.pi * 2,dtype=tf.float64)
+	input_dir = 'data/hitgraphs_big'
+	n_train = 2
+	train_data, valid_data = get_datasets(input_dir, n_train, 2)
+	block = GNN()
 
-	#sys.exit()
+	opt = tf.keras.optimizers.Adam(learning_rate=0.01)
 
-	edge_array = np.array([0.39437991, 0.58592331, 0.71819769, 0.07528009, 0.98749489, 0.21369549]) * 2 * np.pi 
+	for i in range(n_train):
+		X, Ri, Ro, labels = train_data[i]
+		X = map2angle(X)
+		Ri = Ri.astype(X.dtype)
+		Ro = Ro.astype(X.dtype)
+		edge_array = [X,Ri,Ro]
+
+		grads = gradient(block,edge_array,labels)
+		#opt.apply_gradients(zip([grads], [theta_learn]))
+		#out  = block([X,Ri,Ro])
+	print(block.variables[0].shape)
+	block.summary()
+
 	
-	y = 0
-	opt = tf.keras.optimizers.Adam(learning_rate=0.1)
 
-	grad0,grad1 = tf_grad(edge_array,w0,w1,y)
-	print(grad0)
-	print(grad1)
-
-	opt.apply_gradients(zip([grad0,grad1],[w0,w1]))
-	print(w0)
-	print(w1)
-
-	
 
 
 
