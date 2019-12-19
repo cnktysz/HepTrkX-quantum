32e9940ed47894b0b1dac602ae7166bd411e2f75
Cenk Tüysüz <cenktuysuz@gmail.com>
2019-12-19 12:01:59 +0300

    added testing feature

--- trying_things/tensorflow_layer.py
+++ trying_things/tensorflow_layer.py
@@ -12,6 +12,60 @@ from math import ceil
 from tools.tools import *
 from qnetworks.GNN import GNN
 ############################################################################################
+def test(data,n_testing,testing='valid'):
+	t_start = time.time()
+
+	if testing=='valid':
+		if os.path.isfile(log_dir+'log_validation_preds.csv'):
+			os.remove(log_dir+'log_validation_preds.csv')
+		print('Starting testing the validation set with ' + str(n_testing) + ' subgraphs!')
+	if testing=='train':
+		if os.path.isfile(log_dir+'log_training_preds.csv'):
+			os.remove(log_dir+'log_training_preds.csv')
+		print('Starting testing the training set with ' + str(n_testing) + ' subgraphs!')
+
+	preds   = []
+	labels  = []
+
+	for n_test in range(n_testing):
+		graph_array, labels_ = preprocess(data[n_test])
+		labels.append(labels_)
+		preds.append(block(graph_array))
+		
+
+	n_edges      = len(labels)
+	n_class      = [n_edges - sum(labels), sum(labels)]
+	class_weight = [n_edges/(n_class[0]*2), n_edges/(n_class[1]*2)]	
+	accuracy     = mean((1 - abs(preds - labels)) * class_weight[int(labels[i])])
+	loss         = tf.keras.losses.binary_crossentropy(labels,preds) 
+
+	if testing=='valid':
+		#log all preds
+		with open(log_dir+'log_validation_preds.csv', 'a') as f:
+			for i in range(len(preds)):
+				f.write('%.4f, %.4f\n' %(preds[i],labels[i]))
+		#calcualte auc
+		fpr,tpr,thresholds = metrics.roc_curve(preds[:,1].astype(int),preds[:,0],pos_label=1 )
+		auc = metrics.auc(fpr,tpr)			
+		#log
+		with open(log_dir+'log_validation.csv', 'a') as f:
+				f.write('%.4f, %.4f, %.4f\n' %(accuracy,auc,loss))
+		duration = time.time() - t_start
+		print('Validation Loss: %.4f, Validation Acc: %.4f, Validation AUC: %.4f Elapsed: %dm%ds' %(loss, accuracy*100, auc, duration/60, duration%60))
+	if testing=='train':
+		#log all preds
+		with open(log_dir+'log_training_preds.csv', 'a') as f:
+			for i in range(len(preds)):
+				f.write('%.4f, %.4f\n' %(preds[i],labels[i]))
+			#calcualte auc
+		fpr,tpr,thresholds = metrics.roc_curve(preds[:,1].astype(int),preds[:,0],pos_label=1 )
+		auc = metrics.auc(fpr,tpr)			
+		#log
+		with open(log_dir+'log_training.csv', 'a') as f:
+				f.write('%.4f, %.4f, %.4f\n' %(accuracy,auc,loss))
+		duration = time.time() - t_start
+		print('Training Loss: %.4f, Training Acc: %.4f, Training AUC: %.4f Elapsed: %dm%ds' %(loss, accuracy*100, auc, duration/60, duration%60))
+############################################################################################
 def preprocess(data):
 	X,Ro,Ri,y  = data
 	X[:,2]     = np.abs(X[:,2]) # correction for negative z
@@ -37,7 +91,7 @@ if __name__ == '__main__':
 	print('Input dir: ' + input_dir)
 	# Run variables
 	n_files     = 16*100
-	n_valid     = int(n_files * 0.1)
+	n_valid     = int(n_files * 0.01)
 	n_train     = n_files - n_valid	
 	train_list  = [i for i in range(n_train)]
 	lr          = 0.1
@@ -56,6 +110,8 @@ if __name__ == '__main__':
 
 	# Log Learning variables
 	log_tensor_array(block.trainable_variables,log_dir, 'log_learning_variables.csv')
+
+	test(valid_data,n_valid,testing='valid')
 
 	for epoch in range(n_epoch): 
 		shuffle(train_list) # shuffle the order every epoch
@@ -79,7 +135,10 @@ if __name__ == '__main__':
 				f.write('%.4f\n' %loss)	
 			# Log gradients
 			log_tensor_array(grads,log_dir, 'log_grads.csv')
-			
+
+			# Test every TEST_every
+			if (n_step+1)%TEST_every==0:
+					test(valid_data,n_valid,testing='valid')
 	##################### END TRAINING ##################### 
 
 
