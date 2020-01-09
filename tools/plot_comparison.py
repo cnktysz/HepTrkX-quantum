1363ab01734e533d149db306b3f8def127fb12fc
Cenk Tüysüz <cenktuysuz@gmail.com>
2020-01-09 13:21:48 +0300

    plot update

--- tools/plot_comparison.py
+++ tools/plot_comparison.py
@@ -5,8 +5,10 @@ from sklearn import metrics
 import sys
 
 log_location0 = 'logs/tensorflow/ENE/lr_0_1/'
-log_location1 = 'logs/tensorflow/TTN/lr_0_01/'
-png_location = 'png/comparison/'
+log_location1 = 'logs/tensorflow/ENE2/lr_0_1/'
+log_location2 = 'logs/tensorflow/ENE3/lr_0_1/'
+
+png_location = 'png/comparison/lr_0_1/'
 
 
 with open(log_location0+'log_validation.csv', 'r') as f:
@@ -15,14 +17,28 @@ with open(log_location0+'log_validation.csv', 'r') as f:
 with open(log_location1+'log_validation.csv', 'r') as f:
 	reader = csv.reader(f, delimiter=',')
 	valid1 = np.array(list(reader)).astype(float) 
+with open(log_location2+'log_validation.csv', 'r') as f:
+	reader = csv.reader(f, delimiter=',')
+	valid2 = np.array(list(reader)).astype(float) 
+with open(log_location0 + 'log_loss.csv', 'r') as f:
+	reader = csv.reader(f, delimiter=',')
+	loss0 = np.array(list(reader)).astype(float)
+with open(log_location1 + 'log_loss.csv', 'r') as f:
+	reader = csv.reader(f, delimiter=',')
+	loss1 = np.array(list(reader)).astype(float)
+with open(log_location2 + 'log_loss.csv', 'r') as f:
+	reader = csv.reader(f, delimiter=',')
+	loss2 = np.array(list(reader)).astype(float)
 
-
-plt.clf()   
-interval = 50
+interval = 50 
 x0 = [i*interval for i  in range(len(valid0))]
 x1 = [i*interval for i  in range(len(valid1))]
-plt.plot(x0,valid0[:,2],label='E->N->E',c='darkorange')
-plt.plot(x1,valid1[:,2],label='E',c='navy')
+x2 = [i*interval for i  in range(len(valid2))]
+
+plt.clf()   
+plt.plot(x0,valid0[:,2],label='ENE1',c='darkorange')
+plt.plot(x1,valid1[:,2],label='ENE2',c='navy')
+plt.plot(x2,valid2[:,2],label='ENE3',c='red')
 plt.title('Validation Loss')
 plt.xlabel('Update')
 plt.ylabel('Loss')
@@ -30,8 +46,9 @@ plt.legend()
 plt.savefig(png_location+'validation_loss.pdf')
 
 plt.clf()   
-plt.plot(x0,valid0[:,1],label='E->N->E',c='darkorange')
-plt.plot(x1,valid1[:,1],label='E',c='navy')
+plt.plot(x0,valid0[:,1],label='ENE1',c='darkorange')
+plt.plot(x1,valid1[:,1],label='ENE2',c='navy')
+plt.plot(x2,valid2[:,1],label='ENE3',c='red')
 plt.title('Validation AUC')
 plt.xlabel('Update')
 plt.ylabel('AUC')
@@ -39,4 +56,17 @@ plt.legend()
 plt.savefig(png_location+'validation_auc.pdf')
 
 
+x0 = [i+1 for i  in range(len(loss0))]
+x1 = [i+1 for i  in range(len(loss1))]
+x2 = [i+1 for i  in range(len(loss2))]
+
+plt.clf()   
+plt.plot(x0,loss0,label='ENE1',c='darkorange')
+plt.plot(x1,loss1,label='ENE2',c='navy')
+plt.plot(x2,loss2,label='ENE3',c='red')
+plt.title('Validation Loss')
+plt.xlabel('Update')
+plt.ylabel('Loss')
+plt.legend()
+plt.savefig(png_location+'loss.pdf')
 
--- tools/plot_statistics.py
+++ tools/plot_statistics.py
@@ -4,93 +4,114 @@ import csv
 from sklearn import metrics
 import sys
 
-log_location = 'logs/tensorflow/ENE/lr_0_1/'
-png_location = 'png/tensorflow/ENE/lr_0_1/'
+def plot_statistics(log_location,png_location):
+	with open(log_location + 'log_loss.csv', 'r') as f:
+		reader = csv.reader(f, delimiter=',')
+		loss = np.array(list(reader)).astype(float)
+	with open(log_location+'log_validation.csv', 'r') as f:
+		reader = csv.reader(f, delimiter=',')
+		valid = np.array(list(reader)).astype(float) 
+	with open(log_location+'log_learning_variables.csv','r') as f:
+		reader = csv.reader(f, delimiter=',')
+		learning_vars = np.array(list(reader))[:,0:-1].astype(float)
+	with open(log_location+'log_grads.csv','r') as f:
+		reader = csv.reader(f, delimiter=',')
+		grads = np.array(list(reader))[:,0:-1].astype(float)
+	with open(log_location + 'log_validation_preds.csv', 'r') as f:
+		reader = csv.reader(f, delimiter=',')
+		valid_preds = np.array(list(reader)).astype(float)
 
-with open(log_location + 'log_loss.csv', 'r') as f:
-	reader = csv.reader(f, delimiter=',')
-	loss = np.array(list(reader)).astype(float)
-with open(log_location+'log_validation.csv', 'r') as f:
-	reader = csv.reader(f, delimiter=',')
-	valid = np.array(list(reader)).astype(float) 
-with open(log_location+'log_learning_variables.csv','r') as f:
-	reader = csv.reader(f, delimiter=',')
-	learning_vars = np.array(list(reader))[:,0:-1].astype(float)
-with open(log_location+'log_grads.csv','r') as f:
-	reader = csv.reader(f, delimiter=',')
-	grads = np.array(list(reader))[:,0:-1].astype(float)
-with open(log_location + 'log_validation_preds.csv', 'r') as f:
-	reader = csv.reader(f, delimiter=',')
-	valid_preds = np.array(list(reader)).astype(float)
+	# Plots
+	plt.clf()   
+	x = [(i+1) for i  in range(len(loss))]
+	plt.plot(x[1:],loss[1:])
+	plt.xlabel('Update')
+	plt.ylabel('Loss')
+	plt.tight_layout()
+	plt.savefig(png_location+'statistics_loss.pdf')
 
-# Plots
-plt.clf()   
-x = [(i+1) for i  in range(len(loss))]
-plt.plot(x[1:],loss[1:])
-plt.xlabel('Update')
-plt.ylabel('Loss')
-plt.savefig(png_location+'statistics_loss.pdf')
+	# Validation Plots
+	plt.clf()   
+	interval = 50
+	x = [i*interval for i  in range(len(valid))]
+	plt.plot(x,valid[:,2])
+	plt.xlabel('Update')
+	plt.ylabel('Loss')
+	plt.tight_layout()
+	plt.savefig(png_location+'validation_loss.pdf')
 
-# Validation Plots
-plt.clf()   
-interval = 50
-x = [i*interval for i  in range(len(valid))]
-plt.plot(x,valid[:,2])
-plt.xlabel('Update')
-plt.ylabel('Loss')
-plt.savefig(png_location+'validation_loss.pdf')
+	plt.clf()   
+	x = [i*interval for i  in range(len(valid))]
+	plt.plot(x,valid[:,0]*100)
+	plt.xlabel('Update')
+	plt.ylabel('Accuracy')
+	plt.tight_layout()
+	plt.savefig(png_location+'validation_accuracy.pdf')
 
-plt.clf()   
-x = [i*interval for i  in range(len(valid))]
-plt.plot(x,valid[:,0]*100)
-plt.xlabel('Update')
-plt.ylabel('Accuracy')
-plt.savefig(png_location+'validation_accuracy.pdf')
+	plt.clf()   
+	x = [i*interval for i  in range(len(valid))]
+	plt.plot(x,valid[:,1])
+	plt.xlabel('Update')
+	plt.ylabel('AUC')
+	plt.tight_layout()
+	plt.savefig(png_location+'validation_auc.pdf')
+	
+	fpr,tpr,thresholds = metrics.roc_curve(valid_preds[:,1].astype(int),valid_preds[:,0],pos_label=1 )
+	auc = metrics.auc(fpr,tpr)
+	# Plot
+	plt.clf()   
+	plt.plot(fpr,tpr,c='navy')
+	plt.plot([0, 1], [0, 1], color='darkorange', linestyle='--')
+	plt.xlabel('False Positive Rate')
+	plt.ylabel('True Positive Rate')
+	plt.title('ROC Curve')
+	plt.tight_layout()
+	plt.savefig(png_location+'validation_roc.pdf') 
 
-plt.clf()   
-x = [i*interval for i  in range(len(valid))]
-plt.plot(x,valid[:,1])
-plt.xlabel('Update')
-plt.ylabel('AUC')
-plt.savefig(png_location+'validation_auc.pdf')
+	fig, axs = plt.subplots(1,3,figsize=(10,4))
+	x = [i for i  in range(len(learning_vars))]
+	for i in range(3):
+		axs[0].scatter(x,learning_vars[:,i],marker='.',label=r'$\theta_{'+str(i)+'}$')
+		axs[0].set_title('InputNet')
+		axs[0].set_xlabel('Update')
+	for i in range(3,18):
+		axs[1].scatter(x,learning_vars[:,i]%(np.pi*2),marker='.',label=r'$\theta_{'+str(i)+'}$')
+		axs[1].set_title('QuantumEdgeNet')
+		axs[1].set_ylabel('Angle')
+		axs[1].set_xlabel('Update')
+	for i in range(18,41):
+		axs[2].scatter(x,learning_vars[:,i]%(np.pi*2),marker='.',label=r'$\theta_{'+str(i)+'}$')
+		axs[2].set_title('QuantumNodeNet')
+		axs[2].set_ylabel('Angle')
+		axs[2].set_xlabel('Update')
+	fig.tight_layout()
+	fig.savefig(png_location+'statistics_learning_variables.pdf')
 
+	fig, axs = plt.subplots(1,3,figsize=(10,4))
+	x = [(i+1) for i  in range(len(grads))]
+	for i in range(3):
+		axs[0].scatter(x,grads[:,i],marker='.',label=r'$\theta_{'+str(i)+'}$')
+		axs[0].set_title('InputNet')
+		axs[0].set_xlabel('Update')
+	for i in range(3,18):
+		axs[1].scatter(x,grads[:,i],marker='.',label=r'$\theta_{'+str(i)+'}$')
+		axs[1].set_title('QuantumEdgeNet')
+		axs[1].set_ylabel('Angle')
+		axs[1].set_xlabel('Update')
+	for i in range(18,41):
+		axs[2].scatter(x,grads[:,i],marker='.',label=r'$\theta_{'+str(i)+'}$')
+		axs[2].set_title('QuantumNodeNet')
+		axs[2].set_ylabel('Angle')
+		axs[2].set_xlabel('Update')
+	fig.tight_layout()
+	fig.savefig(png_location+'statistics_grads.pdf')
+########################################################
+file_list = ['ENE/lr_0_1/','ENE2/lr_0_1/','ENE3/lr_0_1/']
 
-fpr,tpr,thresholds = metrics.roc_curve(valid_preds[:,1].astype(int),valid_preds[:,0],pos_label=1 )
-auc = metrics.auc(fpr,tpr)
-# Plot
-plt.clf()   
-plt.plot(fpr,tpr,c='navy')
-plt.plot([0, 1], [0, 1], color='darkorange', linestyle='--')
-plt.xlabel('False Positive Rate')
-plt.ylabel('True Positive Rate')
-plt.title('ROC Curve')
-plt.savefig(png_location+'validation_roc.pdf') 
-
-
-fig, axs = plt.subplots(2,2)
-fig.suptitle('Learning Variables')
-x = [i for i  in range(len(learning_vars))]
-for i in range(0,3):
-	axs[0,0].scatter(x,learning_vars[:,i],marker='.',label=r'$\theta_{'+str(i)+'}$')
-for i in range(3,18):
-	axs[0,1].scatter(x,learning_vars[:,i],marker='.',label=r'$\theta_{'+str(i)+'}$')
-for i in range(18,41):
-	axs[1,0].scatter(x,learning_vars[:,i],marker='.',label=r'$\theta_{'+str(i)+'}$')
-for i in range(41,56):
-	axs[1,1].scatter(x,learning_vars[:,i],marker='.',label=r'$\theta_{'+str(i)+'}$')
-fig.savefig(png_location+'statistics_learning_variables.pdf')
-
-fig, axs = plt.subplots(2,2)
-fig.suptitle('Learning Variable Gradients')
-x = [(i+1) for i  in range(len(grads))]
-for i in range(0,3):
-	axs[0,0].scatter(x,grads[:,i],marker='.',label=r'$\theta_{'+str(i)+'}$')
-for i in range(3,18):
-	axs[0,1].scatter(x,grads[:,i],marker='.',label=r'$\theta_{'+str(i)+'}$')
-for i in range(18,41):
-	axs[1,0].scatter(x,grads[:,i],marker='.',label=r'$\theta_{'+str(i)+'}$')
-for i in range(41,56):
-	axs[1,1].scatter(x,grads[:,i],marker='.',label=r'$\theta_{'+str(i)+'}$')
-fig.savefig(png_location+'statistics_grads.pdf')
+for i in range(3):
+	file_name = file_list[i]
+	log_location = 'logs/tensorflow/' + file_name
+	png_location = 'png/tensorflow/' + file_name
+	plot_statistics(log_location,png_location)
 
 
