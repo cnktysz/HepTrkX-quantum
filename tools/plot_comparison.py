4db4ad4cdc437b0150e2ded9209fee45a8aec77d
Cenk Tüysüz <cenktuysuz@gmail.com>
2020-03-06 16:47:00 +0300

    update plots

--- tools/plot_comparison.py
+++ tools/plot_comparison.py
@@ -4,12 +4,16 @@ import csv
 from sklearn import metrics
 import sys
 
-log_location0 = 'logs/comparisons/qgnn/iteration_comparison/it1/'
-log_location1 = 'logs/comparisons/qgnn/iteration_comparison/it2/'
-log_location2 = 'logs/comparisons/qgnn/iteration_comparison/it3/'
+log_loc = 'logs/comparisons/dnn/dim-comparison/'
+log_location0 = log_loc + 'hid1/'
+log_location1 = log_loc + 'hid10/'
+log_location2 = log_loc + 'hid100/'
 
-pdf_location = 'pdf/comparison/qgnn/iteration_comparison/'
+pdf_location = 'pdf/comparison/dnn/dim-comparison/'
 
+label1 = r'$N_{dim}=1$'
+label2 = r'$N_{dim}=10$'
+label3 = r'$N_{dim}=100$'
 
 with open(log_location0+'log_validation.csv', 'r') as f:
 	reader = csv.reader(f, delimiter=',')
@@ -30,15 +34,15 @@ with open(log_location2 + 'log_loss.csv', 'r') as f:
 	reader = csv.reader(f, delimiter=',')
 	loss2 = np.array(list(reader)).astype(float)
 
-interval = 50 
+interval = 200 
 x0 = [i*interval for i  in range(len(valid0))]
 x1 = [i*interval for i  in range(len(valid1))]
 x2 = [i*interval for i  in range(len(valid2))]
 
 plt.clf()   
-plt.plot(x0,valid0[:,2],label='iteration=1',c='darkorange')
-plt.plot(x1,valid1[:,2],label='iteration=2',c='navy')
-plt.plot(x2,valid2[:,2],label='iteration=3',c='red')
+plt.plot(x0,valid0[:,2],label=label1,c='darkorange')
+plt.plot(x1,valid1[:,2],label=label2,c='navy')
+plt.plot(x2,valid2[:,2],label=label3,c='red')
 plt.title('Validation Loss')
 plt.xlabel('Update')
 plt.ylabel('Loss')
@@ -46,9 +50,9 @@ plt.legend()
 plt.savefig(pdf_location+'validation_loss.pdf')
 
 plt.clf()   
-plt.plot(x0,valid0[:,1],label='iteration=1',c='darkorange')
-plt.plot(x1,valid1[:,1],label='iteration=2',c='navy')
-plt.plot(x2,valid2[:,1],label='iteration=3',c='red')
+plt.plot(x0,valid0[:,1],label=label1,c='darkorange')
+plt.plot(x1,valid1[:,1],label=label2,c='navy')
+plt.plot(x2,valid2[:,1],label=label3,c='red')
 plt.title('Validation AUC')
 plt.xlabel('Update')
 plt.ylabel('AUC')
@@ -61,9 +65,9 @@ x1 = [i+1 for i  in range(len(loss1))]
 x2 = [i+1 for i  in range(len(loss2))]
 
 plt.clf()   
-plt.plot(x0,loss0,label='iteration=1',c='darkorange')
-plt.plot(x1,loss1,label='iteration=2',c='navy')
-plt.plot(x2,loss2,label='iteration=3',c='red')
+plt.plot(x0,loss0,label=label1,c='darkorange')
+plt.plot(x1,loss1,label=label2,c='navy')
+plt.plot(x2,loss2,label=label3,c='red')
 plt.title('Validation Loss')
 plt.xlabel('Update')
 plt.ylabel('Loss')
